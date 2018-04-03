2f5e7844078ab0afa407b206a27a00b1c12e720b
MJ <mjosesc@yahoo.com>
2018-04-03 17:55:03 +0200

    ultimos cambios

--- travel.sh
+++ travel.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 sudo apt-get update
-#DEBIAN_FRONTEND=noninteractive sudo -E apt-get -q -y install mysql-server
-#sudo apt-get -y install php5
+sudo apt-get install -y mysql-server php-mysql
+debconf-set-selections <<< "mysql-server mysql-server/root_password password mysql"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password mysql"
 sudo apt-get -y install nodejs
 sudo apt-get -y install nodejs-legacy
 sudo apt-get -y install npm
@@ -13,4 +14,5 @@ echo 'CREATE DATABASE IF NOT EXISTS actividad3 CHARACTER SET Latin1 COLLATE lati
 echo 'USE Actividad3;' >> basededatos.sql
 echo 'CREATE TABLE usuarios (id INT NOT NULL AUTO_INCREMENT,usuario VARCHAR(45) NOT NULL,email VARCHAR(45) NOT NULL,password VARCHAR(80) NOT NULL,hash VARCHAR(80) NULL,PRIMARY KEY (id));' >> basededatos.sql
 cat basededatos.sql
+
 sudo mysql -u root < basededatos.sql

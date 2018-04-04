bfc1d6acaeceb0828a129c319c476571f3a4ca4f
MJ <mjosesc@yahoo.com>
2018-04-04 10:22:53 +0200

    ultima versión funcionando en Vagrant

--- travel.sh
+++ travel.sh
@@ -2,17 +2,19 @@
 
 sudo apt-get update
 sudo apt-get install -y mysql-server php-mysql
-debconf-set-selections <<< "mysql-server mysql-server/root_password password mysql"
-debconf-set-selections <<< "mysql-server mysql-server/root_password_again password mysql"
+debconf-set-selections <<< "mysql-server mysql-server/root_password password 1234"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password 1234"
+sudo dpkg --configure -a
 sudo apt-get -y install nodejs
 sudo apt-get -y install nodejs-legacy
 sudo apt-get -y install npm
-curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
-sudo apt-get install -y nodejs
+sudo npm cache clean -f
+sudo npm install -g n
+sudo n stable
 
-echo 'CREATE DATABASE IF NOT EXISTS actividad3 CHARACTER SET Latin1 COLLATE latin1_spanish_ci;'> basededatos.sql
+echo 'CREATE DATABASE IF NOT EXISTS Actividad3 CHARACTER SET Latin1 COLLATE latin1_spanish_ci;'> basededatos.sql
 echo 'USE Actividad3;' >> basededatos.sql
-echo 'CREATE TABLE usuarios (id INT NOT NULL AUTO_INCREMENT,usuario VARCHAR(45) NOT NULL,email VARCHAR(45) NOT NULL,password VARCHAR(80) NOT NULL,hash VARCHAR(80) NULL,PRIMARY KEY (id));' >> basededatos.sql
+echo 'CREATE TABLE usuarios (id INT NOT NULL AUTO_INCREMENT,usuario VARCHAR(45) NOT NULL,email VARCHAR(45) NOT NULL,password VARCHAR(50) NOT NULL,PRIMARY KEY (id));' >> basededatos.sql
 cat basededatos.sql
 
-sudo mysql -u root < basededatos.sql
+sudo mysql -uroot -p1234 < basededatos.sql
